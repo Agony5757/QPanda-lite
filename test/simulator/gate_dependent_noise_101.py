011eb319e85b34569ac9931674368ff520715a19
Yunjie Wang <yunjiewang8214314@gmail.com>
2023-11-16 23:24:23 +0800

    [Typo.]: Ry implementation
    Changed:
    
    https://qpanda-tutorial.readthedocs.io/zh/latest/QGate.html @automatic-code-ztr
    
    Add example code of QDM using noisy simulator

--- test/simulator/gate_dependent_noise_101.py
+++ test/simulator/gate_dependent_noise_101.py
@@ -8,7 +8,7 @@ from qpandalite.circuit_builder import Circuit
 
 # Define the noise descriptions
 noise_description = {
-    "damping": 0.01
+    "depolarizing": 0.01
 }
 
 # Define the gate noise descriptions
@@ -42,25 +42,9 @@ simulator.rphi90(1, np.pi/8)
 simulator.rphi180(1, np.pi/4)
 simulator.rphi(1, np.pi/2, np.pi/4)
 # Number of measurement shots
-shots = 1
+shots = 1024
 
 # # Measure the state multiple times
 measurement_results = simulator.measure_shots(shots)
 
-import qpandalite.simulator as qsim
-
-sim = qsim.Simulator()
-
-sim.init_n_qubit(2)
-sim.hadamard(0, False)
-sim.x(0, False)
-sim.hadamard(1, False)
-sim.z(1, False)
-sim.iswap(0, 1, False)
-sim.rx(0, np.pi/8, False)
-sim.ry(0, np.pi/4, False)
-sim.rz(0, np.pi/2, False)
-sim.rphi90(1, np.pi/8, False)
-sim.rphi180(1, np.pi/4, False)
-sim.rphi(1, np.pi/2, np.pi/4, False)
-print(sim.state)
+print(measurement_results)

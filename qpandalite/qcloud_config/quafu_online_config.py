f9ec64b019d8f1d5fc74f1b26d45c2f1dbd83829
Chaos-Paradox <72609440+Chaos-Paradox@users.noreply.github.com>
2024-03-29 11:37:30 +0800

    FIX: Added an interface to modify the path of current working directory(Path.cwd). (#46)
    Co-authored-by: Pioneer <zzt@orginqc.com>

--- qpandalite/qcloud_config/quafu_online_config.py
+++ qpandalite/qcloud_config/quafu_online_config.py
@@ -1,16 +1,20 @@
 import json
 from pathlib import Path
 
-def create_quafu_online_config(default_token = None):    
+def create_quafu_online_config(default_token = None,
+                               savepath = None):    
 
     if not default_token:
         raise RuntimeError('You should input your token here.')
+
+    if not savepath:
+        savepath = Path.cwd()
 
     default_online_config = {
         'default_token' : default_token,
     }
 
-    with open(Path.cwd() / 'quafu_online_config.json', 'w') as fp:
+    with open(savepath / 'quafu_online_config.json', 'w') as fp:
         json.dump(default_online_config, fp)
 
 if __name__ == '__main__':

63105ad2a9f43807dc9ac9cfedf08113b77d6258
Agony <survivalonlysoul@163.com>
2025-01-17 15:17:58 +0800

    Update test_QASMBench.py

--- qpandalite/test/test_QASMBench.py
+++ qpandalite/test/test_QASMBench.py
@@ -3,7 +3,7 @@ import qpandalite.simulator as qsim
 import numpy as np
 
 import qpandalite.simulator as sim
-from qpandalite.qasm_origin import OpenQASM2_Parser
+from qpandalite.qasm import OpenQASM2_BaseParser, OpenQASM2_LineParser
 from qpandalite.circuit_builder import Circuit
 from pathlib import Path
 import pickle

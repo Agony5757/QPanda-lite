45ae46018b1246b028b0917c13cf355aa1ba59f9
Agony5757 <chenzhaoyun94@gmail.com>
2026-04-09 00:03:31 +0800

    fix: provide math.pi in eval() namespace for handle_parameters

--- qpandalite/qasm/qasm_line_parser.py
+++ qpandalite/qasm/qasm_line_parser.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 __all__ = ["OpenQASM2_LineParser"]
 
+import math
 import re
 from typing import Any
 
@@ -90,7 +91,7 @@ class OpenQASM2_LineParser:  # noqa: N801
         parameter_str_list = parameters_str.split(",")
         parameters: list[float] = []
         for parameter_str in parameter_str_list:
-            parameters.append(float(eval(parameter_str.strip())))  # noqa: PGH001, S307
+            parameters.append(float(eval(parameter_str.strip(), {"pi": math.pi})))  # noqa: PGH001, S307
         return parameters
 
     @staticmethod
